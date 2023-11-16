08a62150d7293a2ef182e367903f547189f366fc
izaaaliine <izaline.18@gmail.com>
2023-11-16 11:08:30 +0100

    correction exercice

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// Mon exercice ---------
+
 let faceDe = Math.floor(Math.random() * 6) + 1;
 console.log("Résultat du lancer de dé :", faceDe);
 
@@ -5,48 +7,66 @@ const body = document.body;
 
 let img = new Image();
 
-if (faceDe === 1) {
+if (faceDe == 1) {
   img.src = "img/image1.png";
-} else if (faceDe === 2) {
+} else if (faceDe == 2) {
   img.src = "img/image2.png";
-} else if (faceDe === 3) {
+} else if (faceDe == 3) {
   img.src = "img/image3.png";
-} else if (faceDe === 4) {
+} else if (faceDe == 4) {
   img.src = "img/image4.png";
-} else if (faceDe === 5) {
+} else if (faceDe == 5) {
   img.src = "img/image5.png";
-} else if (faceDe === 6) {
+} else if (faceDe == 6) {
   img.src = "img/image6.png";
 }
 body.appendChild(img);
 
-// faceDe=2 ne marchait pas voici la solution
-// ChatGPT
-// Il semble y avoir une confusion dans les conditions if. Vous utilisez actuellement l'opérateur d'assignation (=) au lieu de l'opérateur de comparaison (===). L'opérateur = attribue la valeur à faceDe plutôt que de vérifier son contenu. En conséquence, toutes les conditions seront toujours vraies, et seule la première branche sera exécutée.
+// // Correction exercice ---------------------------
+// // mettre image dans le body avec une "classe : tailleDe"
 
-// Modifiez vos conditions pour utiliser === pour comparer les valeurs au lieu de les attribuer.
+// let nombre = Math.floor(Math.random()*6 +1)
+// const de = document.querySelector(".tailleDe")
+// de.src = `img/de${nombre}.png`
+// // la source de l'image change en fonction de la variable nombre
 
-// CODE CHATGTP
-// let faceDe = Math.floor(Math.random() * 6) + 1;
+// // Correction Bonus ---------------------------
+// // 3 images : contour6, contour12, contour20
 
-// // Récupérer la référence au corps du document
-// const body = document.body;
+// const de6 = document.querySelector('#de6');
+// const de12 = document.getElementById('de12');
+// const de20 = document.getElementById('de20');
+// const de = document.querySelector(".tailleDe")
+//     // variables des input
 
-// // Tableau des chemins des images
-// const cheminsImages = [
-//   "img/1.png",
-//   "img/2.png",
-//   "img/3.png",
-//   "img/4.png",
-//   "img/5.png",
-//   "img/6.png",
-// ];
+// // Comprendre comment sélectionner avec le bouton radio la bonne valeur
+// let typeDe
 
-// // Créer un élément image
-// let img = document.createElement("img");
+// const lancerUnDe = document.querySelector('.lancerUnDe');
 
-// // Définir le chemin de l'image en fonction du résultat du lancer de dé
-// img.src = cheminsImages[faceDe - 1]; // Soustrayez 1 car les tableaux sont indexés à partir de 0
-
-// // Ajouter l'image au corps du document
-// body.appendChild(img);
+// lancerUnDe.addEventListener('mouseover', (event) => {
+//   event.preventDefault();
+//       // écouteurs d'évenement preventDefault permet de supprimer le comportement par défaut c-a-d au mouseour (on peut en mettre plein, click..) il se passe ça
+//   let deRadio = document.getElementsByName("de");
+//   for (let i = 0; i < deRadio.length; i++) {
+//     if (deRadio[i].checked) {
+//       typeDe = deRadio[i].value;
+//     }
+//   }
+//       // permet d'affecter la variable typeDe à la value de l'input donc que le chiffre aille de (1 à 6) de (1 à 12) ou (1 à 20)
+//   let nombre = Math.floor(Math.random() * typeDe + 1);
+//   if (de6.checked) {
+//     de.src = "img/contour6.png";
+//   }
+//   if (de12.checked) {
+//     de.src = "img/contour12.png";
+//   }
+//   if (de20.checked) {
+//     de.src = "img/contour20.png";
+//   }
+//       // faire en sorte que l'image s'adapte au dé checked 6, 12 ou 20
+//   const selectP = document.querySelector(".nombreDe");
+//   selectP.textContent = nombre;
+//       // pour que le chiffre dans <p> change en fonction du nombre random
+//   console.log(nombre);
+// })
